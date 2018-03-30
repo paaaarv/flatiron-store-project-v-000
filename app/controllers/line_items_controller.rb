ff0ea02558041da692c5ac6c8bbd168800cbb480
Parv Kumar <parvathikumar92@gmail.com>
2018-03-30 20:56:03 +0000

    added line_items create route

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,9 @@
 class LineItemsController < ApplicationController
 
+  def create
+    @line_items = LineItem.find_or_create_by(item_id: params[:item_id])
+    @line_items.cart_id = params[:cart_id]
+    @line_items.save
+    redirect_to '/'
+  end
 end
